0f15f28bf6efe226f1d4c95cd3583e8943acaea2
keira-park <ynnus411@naver.com>
2021-11-02 19:41:24 +0900

    Add login router in index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const {User} = require('./models/User');
 
 const config = require('./config/key');
@@ -39,6 +40,35 @@ app.post('/register', (req, res) => {
     })
 })
 
+app.post('/login', (req, res) => {
+
+    // 요청된 이메일이 데이터 베이스에서 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+    // 요청된 이메일이 데이터 베이스에 있다면, 비밀 번호가 맞는 비번인지 확인
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({
+                    loginSuccess: false,
+                    message: "비밀번호가 틀렸습니다."
+                })
+            // 비밀번호까지 같다면 token을 생성하기.
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                //토큰을 저장한다. 어디에? 쿠키, 로컬스토리지, ...
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id})
+            })
+        })
+    })
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
