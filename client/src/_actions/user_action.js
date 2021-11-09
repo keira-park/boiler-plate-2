1f909745e22cf1d846d2278baa5db8f10a4e2504
keira-park <ynnus411@naver.com>
2021-11-09 19:24:51 +0900

    Create _actions folder

---
+++ client/src/_actions/user_action.js
@@ -0,0 +1,17 @@
+import axios from 'axois';
+import {
+    LOGIN_USER
+} from './types';
+
+
+export function loginUser(dataToSubmit) {
+    // 서버에 request를 날린 후 response 받은 거에다가
+    // response.data, 서버에서 받은 데이터를 request에 저장.
+    const request = axios.post('/api/users/login', dataToSubmit)
+        .then(response => response.data)
+     
+    return {
+        type: LOGIN_USER,
+        payload: request
+    }
+}
