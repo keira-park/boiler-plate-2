98168eb1c2c15450fe3f0233598a3ada105c61f2
keira-park <ynnus411@naver.com>
2021-11-09 23:37:51 +0900

    Cleared the typos

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -2,10 +2,10 @@ import {
     LOGIN_USER
 } from '../_actions/types';
 
-export default function (state, action) {
+export default function Reducer(state={}, action){
     switch (action.type) {
         case LOGIN_USER:
-            return {...state, loginSuccess: action.payload }
+            return {...state, loginSuccess: action.payload}
         default:
             return state;
     }
