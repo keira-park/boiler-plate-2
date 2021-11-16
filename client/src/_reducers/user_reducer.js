3c8bab4b8f8f2d3f5832646b076d9e5168dcea69
keira-park <ynnus411@naver.com>
2021-11-16 20:50:27 +0900

    Add case REGISTER_USER and AUTH_USER

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,11 +1,17 @@
 import {
-  LOGIN_USER
+  LOGIN_USER,
+  REGISTER_USER,
+  AUTH_USER
 } from '../_actions/types';
 
 export default function Reducer(state={}, action){
   switch (action.type) {
     case LOGIN_USER:
       return {...state, loginSuccess: action.payload}
+    case REGISTER_USER:
+      return {...state, register: action.payload}
+    case AUTH_USER:
+      return {...state, userData: action.payload}
     default:
       return state;
   }
